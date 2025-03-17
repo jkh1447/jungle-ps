5b4aeb682bc94cd925af792fba44962d21f233a1
jkh1447 <jkh1447@gmail.com>
2025-03-17 15:06:33 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 42024 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,9 +1,9 @@
 import math
-
-n, r, c = list(map(int, input().split()))
+import sys
+import queue
+import copy
 
 def func(n, r, c):
-    # 함수의 정의 : 2^n 크기의 배열에서 r, c 의 방문순서
     if n == 1:
         if r == 0 and c == 0:
             return 0
@@ -14,16 +14,16 @@ def func(n, r, c):
         else:
             return 3
         
-    if r < 2**(n-1) and c < 2**(n-1):
-        #1사분면 
+    half = 2**n >> 1
+    if r < half and c < half:
         return func(n-1, r, c)
-    elif r < 2**(n-1) and c >= 2**(n-1):
-        #2사분면
-        return 1 * 2**(n-1) * 2**(n-1) + func(n-1, r, c-2**n / 2)
-    elif r >= 2**(n-1) and c < 2**(n-1):
-        #3사분면
-        return 2 * 2**(n-1) * 2**(n-1) + func(n-1, r-2**n / 2, c)
+    elif r < half and c >= half:
+        return func(n-1, r, c - half) + half * half
+    elif r >= half and c < half:
+        return func(n-1, r - half, c) + 2 * half * half
     else:
-        return 3 * 2**(n-1) * 2**(n-1) + func(n-1, r-2**n / 2, c-2**n / 2)
+        return func(n-1, r - half, c - half) + 3 * half * half
     
+n, r, c = list(map(int, sys.stdin.readline().split()))
+
 print(func(n, r, c))
