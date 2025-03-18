acbfc64113320904d9b34514c0a45c098b671f05
jkh1447 <jkh1447@gmail.com>
2025-03-18 22:29:41 +0900

    [Gold IV] Title: N-Queen, Time: 20140 ms, Memory: 40988 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,28 @@
 import math
+import sys
+import queue
+import copy
+from itertools import combinations
 
 n = int(input())
 
-lst1 = [False for x in range(n+1)] #열
-lst2 = [False for x in range(2*n+1)] #기울기-1
-lst3 = [False for x in range(2*n+1)] #기울기 1
-
+lst1 = [False for _ in range(20)]
+lst2 = [False for _ in range(50)]
+lst3 = [False for _ in range(50)]
 ans = 0
 def func(r):
-    #print(f"{r}")
+    global ans
+    if r == n:
+        ans+=1
     for i in range(n):
-        if not lst1[i] and not lst2[r-i+n-1] and not lst3[r+i]:
-            if r == 0:
-                global ans
-                ans +=1
-                return
+        if not lst1[i] and not lst2[r + i] and not lst3[r - i + n - 1]:
             lst1[i] = True
-            lst2[r-i+n-1] = True
-            lst3[r+i] = True
-            func(r-1)
+            lst2[r + i] = True
+            lst3[r - i + n - 1] = True
+            func(r+1)
             lst1[i] = False
-            lst2[r-i+n-1] = False
-            lst3[r+i] = False
-func(n-1)
+            lst2[r + i] = False
+            lst3[r - i + n - 1] = False
+
+func(0)
 print(ans)
