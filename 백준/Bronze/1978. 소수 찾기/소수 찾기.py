73aec378adc516f79948258950bdde234effc664
jkh1447 <jkh1447@gmail.com>
2025-03-18 21:24:17 +0900

    [Bronze II] Title: 소수 찾기, Time: 88 ms, Memory: 41268 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -2,18 +2,21 @@ import math
 import sys
 import queue
 import copy
+from itertools import combinations
 
 def isPrime(n):
-    if n == 1: return False
-    for i in range(2, n):
-        if n % i == 0:
+    if n == 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n%i ==0:
             return False
     return True
+n = int(sys.stdin.readline().strip())
+lst = list(map(int, sys.stdin.readline().strip().split()))
 
-n = int(input())
+
 ans = 0
-lst = list(map(int, sys.stdin.readline().split()))
 for i in lst:
     if isPrime(i):
-        ans += 1
+        ans+=1
 print(ans)
