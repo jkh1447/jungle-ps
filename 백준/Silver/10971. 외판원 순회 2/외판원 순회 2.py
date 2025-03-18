4c22cdc829f4f0dde69f1409a320f57d6fcaf128
jkh1447 <jkh1447@gmail.com>
2025-03-18 22:07:39 +0900

    [Silver II] Title: 외판원 순회 2, Time: 6340 ms, Memory: 41952 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -2,38 +2,35 @@ import math
 import sys
 import queue
 import copy
+from itertools import combinations
 
-
-n = int(sys.stdin.readline().strip())
-
+n = int(input())
 cost = []
-
-for _ in range(n):
-    cost.append(list(map(int, sys.stdin.readline().strip().split())))
-
-min = sys.maxsize
-
-def tsp(visited, path, hap):
-    global min
-
+min_val = sys.maxsize
+def func(path, visited, hap):
+    global min_val
     if len(path) == n:
-        if cost[path[-1]][path[0]] == 0:
+        if cost[path[-1]][path[0]] != 0:
+            hap += cost[path[-1]][path[0]]
+            if min_val > hap:
+                min_val = hap
+            hap -= cost[path[-1]][path[0]]
             return
-        hap += cost[path[-1]][path[0]]
-        if hap < min:
-            min = hap
-        hap -= cost[path[-1]][path[0]]
-        return
     for i in range(n):
         if not visited[i] and (len(path) == 0 or cost[path[-1]][i] != 0):
             visited[i] = True
             path.append(i)
             if len(path) > 1:
                 hap += cost[path[-2]][path[-1]]
-            tsp(visited, path, hap)
+            func(path, visited, hap)
             if len(path) > 1:
                 hap -= cost[path[-2]][path[-1]]
-            visited[i] = False
             path.pop()
-tsp([False for _ in range(n)], [], 0)
-print(min)
+            visited[i] = False
+
+for _ in range(n):
+    cost.append(list(map(int, sys.stdin.readline().strip().split())))
+
+func([], [False for _ in range(n)], 0)
+print(min_val)
+
