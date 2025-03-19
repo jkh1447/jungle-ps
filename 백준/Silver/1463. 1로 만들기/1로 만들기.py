22af38126f96fedb10d6ae40a97dde27adedd7aa
jkh1447 <jkh1447@gmail.com>
2025-03-19 23:28:34 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 42580 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,23 @@
-n = int(input())
-dp = [0] * 1000001
-dp[0] = 9999999
-dp[1] = 0
-dp[2] = 1
-dp[3] = 1
-for i in range(4, n+1):
-    if i % 3 == 0 and i % 2 == 0:
-        dp[i] = min(dp[i-1], dp[i//2], dp[i//3])+1
-    elif i % 2 == 0:
-        dp[i] = min(dp[i - 1], dp[i // 2])+1
-    elif i % 3 == 0:
-        dp[i] = min(dp[i - 1], dp[i // 3])+1
-    else:
-        dp[i] = dp[i - 1]+1
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+visited = [0 for i in range(N+1)]
+def bfs():
+    q = deque()
+    q.append(N)
+    while q:
+        cur = q.popleft()
+        if cur==1:
+            break
+        if cur%3==0 and visited[cur//3]==0:
+            visited[cur//3] = visited[cur] + 1
+            q.append(cur//3)
+        if cur%2 == 0 and visited[cur//2]==0:
+            visited[cur//2] = visited[cur] + 1
+            q.append(cur//2)
+        if visited[cur-1]==0:
+            visited[cur-1] = visited[cur] + 1
+            q.append(cur-1)
 
-print(dp[n])
+bfs()
+print(visited[1])
