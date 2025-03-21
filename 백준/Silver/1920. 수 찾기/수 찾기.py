3c10ae20f6dc9a56cc42da8ea34454bd2b6df022
jkh1447 <jkh1447@gmail.com>
2025-03-21 14:37:46 +0900

    [Silver IV] Title: 수 찾기, Time: 472 ms, Memory: 52940 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,33 +1,29 @@
 import math
 import sys
 from collections import deque
-
+import heapq
 import copy
 from itertools import combinations
 sys.setrecursionlimit(100000)
 
 n = int(input())
-A = list(map(int, sys.stdin.readline().strip().split()))
+lst1 = list(map(int, sys.stdin.readline().strip().split()))
 m = int(input())
-B = list(map(int, sys.stdin.readline().strip().split()))
-A.sort()
+lst2 = list(map(int, sys.stdin.readline().strip().split()))
+lst1.sort()
 
-
-def binarySearch(arr, num):
-
+def bs(arr, num):
     st = 0
     en = n-1
-
     while st <= en:
-        mid = (st+en) // 2
-        if num == arr[mid]:
-            return 1
-        if num > arr[mid]:
+        mid = (st+en)//2
+        if arr[mid] > num:
+            en = mid - 1
+        elif arr[mid] < num:
             st = mid + 1
         else:
-            en = mid - 1
-
+            return 1
     return 0
-
-for b in B:
-    print(binarySearch(A, b))
+for i in range(m):
+    print(bs(lst1, lst2[i]))
+        
