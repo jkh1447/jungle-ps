84ab0f40afe58c79d056ac6d834671986fa35423
jkh1447 <jkh1447@gmail.com>
2025-03-22 21:54:06 +0900

    [Gold IV] Title: 공유기 설치, Time: 948 ms, Memory: 46092 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -7,28 +7,30 @@ from itertools import combinations
 sys.setrecursionlimit(100000)
 
 n, c = list(map(int, sys.stdin.readline().strip().split()))
-lst = []
+homes = []
+
 for _ in range(n):
-    lst.append(int(sys.stdin.readline().strip()))
+    homes.append(int(sys.stdin.readline().strip()))
 
-lst.sort()
+homes.sort()
 
-st = 1
-en = lst[-1]
-result = 0
+max = homes[-1]
+
+st = 0
+en = max
+ans = 0
 while st <= en:
     mid = (st+en)//2
-
-    cur = 0
     cnt = 1
+    cur = 0
     for i in range(1, n):
-        if lst[cur] + mid <= lst[i]:
+        if homes[i] - homes[cur] >= mid: #전부 다 mid값보다 크거나 같은 차이로 공유기 설치에 성공하면 인접한 두 공유기의 거리가 최소한 mid이상이 된다
             cnt += 1
             cur = i
     if cnt >= c:
-        result = mid
-
+        ans = mid
         st = mid + 1
-    else:
+    else: 
         en = mid - 1
-print(result)
+
+print(ans)
