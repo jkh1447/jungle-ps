d5151dc765df3bb91d373c5302cba1f927b38fc8
jkh1447 <jkh1447@gmail.com>
2025-03-22 23:26:01 +0900

    [Gold V] Title: 두 용액, Time: 516 ms, Memory: 48592 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -5,37 +5,35 @@ import heapq
 import copy
 from itertools import combinations
 sys.setrecursionlimit(100000)
-
 n = int(input())
+sol = list(map(int, sys.stdin.readline().strip().split()))
 
-lst = list(map(int, sys.stdin.readline().strip().split()))
-lst.sort()
-
+sol.sort()
 ai = 0
 aj = 0
-min_val = float('inf')
-
-for i in range(n):
-    target = -lst[i]
-
+min_val = (1 << 37)
+for i in range(n-1):
+    target = -sol[i]
     st = i+1
     en = n-1
-
+    #en을 n-1까지하면 target넘버가 가장 큰 수일 경우 n-1을 반환
     while st < en:
-        mid = (st+en)//2
-        if lst[mid] >= target:
+        mid=(st+en)//2
+
+        if sol[mid] >= target:
             en = mid
         else:
             st = mid + 1
-    
-    if i+1 <= st and st < n and abs(lst[st] + lst[i]) < min_val:
-        min_val = abs(lst[st] + lst[i])
+    # i<=st면 i == st가 돼서 x,
+    # i가 n-1이 되면 st가 n이 되고 while문은 스킵되면서 st = n이 된다. 이 경우는 고려하지 않아도 된다
+    if i+1 <= st and abs(sol[st] + sol[i]) < min_val:
+        min_val = abs(sol[st] + sol[i])
         ai = i
         aj = st
-    
-    if i+1 <= st - 1 and st - 1 < n and abs(lst[st-1] + lst[i]) < min_val:
-        min_val = abs(lst[st-1] + lst[i])
+    # i+1 == st이면 st-1이 i가 되버리기 때문
+    if i+1 <= st - 1  and abs(sol[st-1]+sol[i]) < min_val:
+        min_val = abs(sol[st-1] + sol[i])
         ai = i
-        aj = st-1
+        aj = st - 1
+print(f"{sol[ai]} {sol[aj]}")
 
-print(f"{lst[ai]} {lst[aj]}")
