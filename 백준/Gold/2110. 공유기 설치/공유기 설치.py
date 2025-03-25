c100c01a1300bfc2ad730fa6f06bb31c020c4ea0
jkh1447 <jkh1447@gmail.com>
2025-03-25 20:41:13 +0900

    [Gold IV] Title: 공유기 설치, Time: 896 ms, Memory: 45552 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -13,24 +13,24 @@ for _ in range(n):
     homes.append(int(sys.stdin.readline().strip()))
 
 homes.sort()
-
-max = homes[-1]
+max_val = homes[-1]
 
 st = 0
-en = max
+en = max_val
 ans = 0
-while st <= en:
+while st<=en:
     mid = (st+en)//2
+
     cnt = 1
-    cur = 0
+    current = homes[0]
     for i in range(1, n):
-        if homes[i] - homes[cur] >= mid: #전부 다 mid값보다 크거나 같은 차이로 공유기 설치에 성공하면 인접한 두 공유기의 거리가 최소한 mid이상이 된다
+        if (homes[i] - current) >= mid:
             cnt += 1
-            cur = i
+            current = homes[i]
+
     if cnt >= c:
         ans = mid
         st = mid + 1
-    else: 
+    else:
         en = mid - 1
-
 print(ans)
