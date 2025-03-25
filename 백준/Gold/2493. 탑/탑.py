f897e4964caccb374e1b79519edbf44b3009c95a
jkh1447 <jkh1447@gmail.com>
2025-03-25 23:04:05 +0900

    [Gold V] Title: 탑, Time: 696 ms, Memory: 119596 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,9 +1,10 @@
 import math
 import sys
-import queue
-
+from collections import deque
+import heapq
 import copy
 from itertools import combinations
+sys.setrecursionlimit(100000)
 
 n = int(input())
 lst = list(map(int, sys.stdin.readline().strip().split()))
@@ -14,8 +15,9 @@ s.append([0, 1 << 27])
 for i in range(n):
     while True:
         if lst[i] < s[-1][1]:
-            print(s[-1][0], end=" ")
+            print(s[-1][0], end=' ')
             break
         else:
             s.pop()
     s.append([i+1, lst[i]])
+
