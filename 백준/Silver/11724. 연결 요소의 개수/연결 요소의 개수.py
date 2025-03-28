a505cc6d9ede9ec34cef88c191dc50cc5ebc4382
jkh1447 <jkh1447@gmail.com>
2025-03-28 15:10:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 828 ms, Memory: 71388 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -5,30 +5,30 @@ import heapq
 import copy
 from itertools import combinations
 sys.setrecursionlimit(100000)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 n, m = list(map(int, sys.stdin.readline().strip().split()))
 
-vertices = [[] for _ in range(n+1)]
+adj = [[] for _ in range(n+1)]
 
-for _ in range(m):
+for i in range(m):
     u, v = list(map(int, sys.stdin.readline().strip().split()))
-    vertices[u].append(v)
-    vertices[v].append(u)
+    adj[u].append(v)
+    adj[v].append(u)
 visited = [0 for _ in range(n+1)]
-
 ans = 0
 for i in range(1, n+1):
-    q = deque()
     if not visited[i]:
+        ans += 1
+        q = deque()
         q.append(i)
         visited[i] = 1
 
         while q:
             cur = q.popleft()
-            # print(f"{cur} ->", end=' ')
-            for next in vertices[cur]:
-                if not visited[next]:
-                    visited[next] = 1
-                    q.append(next)
-        ans+=1
+            for nxt in adj[cur]:
+                if not visited[nxt]:
+                    visited[nxt] = 1
+                    q.append(nxt)
 print(ans)
