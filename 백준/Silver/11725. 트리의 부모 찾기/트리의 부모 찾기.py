22e7cdb62ff84a48c033ef04e61456e22662816a
jkh1447 <jkh1447@gmail.com>
2025-03-28 16:49:42 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 364 ms, Memory: 55920 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -5,22 +5,29 @@ import heapq
 import copy
 from itertools import combinations
 sys.setrecursionlimit(100000)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 n = int(input())
+
 adj = [[] for _ in range(n+1)]
-for i in range(n-1):
+
+for _ in range(n-1):
     u, v = list(map(int, sys.stdin.readline().strip().split()))
     adj[u].append(v)
     adj[v].append(u)
 
-
+q = deque()
+q.append(1)
 p = [0 for _ in range(n+1)]
-def dfs(x):
-    for nxt in adj[x]:
-        if p[x] == nxt: continue
-        p[nxt] = x
-        dfs(nxt)
-dfs(1)
+p[1] = 1
+
+while q:
+    cur = q.popleft()
+    for nxt in adj[cur]:
+        if p[cur] == nxt: continue
+        p[nxt] = cur
+        q.append(nxt)
 
 for i in range(2, n+1):
     print(p[i])
