b0341cad75b6a092aab40cbae4d0fddc384fa921
jkh1447 <jkh1447@gmail.com>
2025-04-03 22:22:51 +0900

    [Gold V] Title: 회의실 배정, Time: 352 ms, Memory: 252972 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,28 @@
+import math
+import sys
+from collections import deque
+import heapq
+import copy
+from itertools import combinations
+sys.setrecursionlimit(100000)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+arr = []
+n = int(input())
+for _ in range(n):
+    s, e = list(map(int, sys.stdin.readline().strip().split()))
+    arr.append((s, e))
+arr.sort(key=lambda x: (x[1], -(x[1]-x[0])))
+ans = 1
+
+
+cur_end = arr[0][1]
+for i in range(1, n):
+    #print(f"{arr[i][0]} {cur_end}")
+    if arr[i][0] < cur_end:
+        continue
+    ans+=1
+    cur_end = arr[i][1]
+
+print(ans)
