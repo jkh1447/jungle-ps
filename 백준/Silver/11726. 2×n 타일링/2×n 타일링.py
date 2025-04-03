0e6e0f766353b3a62a155d50241a03a484822a08
jkh1447 <jkh1447@gmail.com>
2025-04-03 17:54:32 +0900

    [Silver III] Title: 2×n 타일링, Time: 136 ms, Memory: 240536 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,6 +1,17 @@
+import math
+import sys
+from collections import deque
+import heapq
+import copy
+from itertools import combinations
+sys.setrecursionlimit(100000)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
 n = int(input())
 
-dp = [0] * 1001
+dp = [0 for _ in range(1001)]
+
 dp[1] = 1
 dp[2] = 2
 
