3e18f8da78963adc4468219ce7c08840b1fc45e7
jkh1447 <jkh1447@gmail.com>
2025-04-09 00:07:12 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 148 ms, Memory: 240536 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,14 +1,23 @@
-form = input()
-
-sum = form.split("-")
-result = 0
-
-for i in sum[0].split("+"):
-    result += int(i)
-
-for i in sum[1:]:
-    for j in i.split("+"):
-        result -= int(j)
+import math
+import sys
+from collections import deque
+import heapq
+import copy
+from itertools import combinations
+sys.setrecursionlimit(100000)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 
-print(result)
+
+tmpexpr = list(sys.stdin.readline().strip().split('-'))
+
+result = []
+for expr in tmpexpr:
+    tmp = map(int, expr.split('+'))
+    result.append(sum(tmp))
+
+ans = result[0]
+for i in range(1, len(result)):
+    ans -= result[i]
+print(ans)
