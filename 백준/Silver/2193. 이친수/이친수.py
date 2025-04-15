1dfce45215feb315d1d25519581fec9a4d681574
jkh1447 <jkh1447@gmail.com>
2025-04-15 11:33:32 +0900

    [Silver III] Title: 이친수, Time: 128 ms, Memory: 240536 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,23 @@
+import math
+import sys
+from collections import deque
+import heapq
+import copy
+from itertools import combinations
+sys.setrecursionlimit(100000)
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+n = int(input())
+
+dp=[0 for _ in range(n+1)]
+
+
+if n < 3:
+    print(1)
+else:
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[n])
