ba9c9609d008fea2482923bf982f09d6d6661d10
jkh1447 <jkh1447@gmail.com>
2025-06-17 10:44:26 +0900

    [Silver V] Title: 배열 합치기, Time: 424 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int A[2000000];
+int A[1000000];
+int B[1000000];
+int C[2000000];
+
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -10,10 +13,34 @@ int main(void) {
 	int an, bn;
 	cin >> an >> bn;
 
-	for (int i = 0; i < an+bn; i++) cin >> A[i];
-	sort(A, A + an + bn);
-	for (int i = 0; i < an + bn; i++) {
-		cout << A[i] << " ";
-	}
+	for (int i = 0; i < an; i++) cin >> A[i];
+	for (int i = 0; i < bn; i++) cin >> B[i];
 
+	int cur = 0;
+	int cura = 0, curb = 0;
+	while (cur != an + bn) {
+		if (cura == an) {
+			C[cur] = B[curb];
+			curb++;
+			cur++;
+			continue;
+		}
+		else if(curb == bn) {
+			C[cur] = A[cura];
+			cura++;
+			cur++;
+			continue;
+		}
+
+		if (A[cura] >= B[curb]) {
+			C[cur] = B[curb];
+			curb++;
+		}
+		else {
+			C[cur] = A[cura];
+			cura++;
+		}
+		cur++;
+	}
+	for (int i = 0; i < an + bn; i++) cout << C[i] << " ";
 }
