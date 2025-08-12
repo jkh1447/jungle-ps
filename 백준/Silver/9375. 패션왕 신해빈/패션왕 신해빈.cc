2c138412c75057f0c4d33ead8885ff8a09b70183
punch <jkh1447@gmail.com>
2025-08-12 15:41:31 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+
+//int dx[6] = { 0, 1, 0, -1, 0, 0 };
+//int dy[6] = { 1, 0, -1, 0, 0, 0 };
+//int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int T;
+	cin >> T;
+
+	for (int tc = 0; tc < T; tc++) {
+		int n;
+		cin >> n;
+		unordered_map<string, vector<string>> clst;
+		for (int i = 0; i < n; i++) {
+			string name, type;
+
+			cin >> name >> type;
+			clst[type].push_back(name);
+		}
+		long long ans = 1;
+		for (auto cur = clst.begin(); cur != clst.end(); cur++) {
+			ans *= ((*cur).Y.size() + 1);
+		}
+		if (n == 0) cout << 0 << "\n";
+		else cout << ans - 1 << "\n";
+	}
+	
+
+}
