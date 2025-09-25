1d8cb38f07420b6550bca0f8dc1f0ecafaa6f600
punch <jkh1447@gmail.com>
2025-09-25 18:43:35 +0900

    [level 0] Title: 배열 조각하기, Time: 0.91 ms, Memory: 6.67 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> query) {
-    vector<int> answer;
     
+    int s = 0, e = arr.size() - 1;
     for(int i=0; i<query.size(); i++){
         if(i%2 == 0){
-            arr.erase(arr.begin() + query[i] + 1, arr.begin() + arr.size());
+            e = s + query[i];
         }
         else {
-            arr.erase(arr.begin(), arr.begin() + query[i]);
+            s += query[i];
         }
     }
     
-    answer = arr;
     
+    vector<int> answer(arr.begin() + s, arr.begin() + e + 1);
     
     return answer;
 }
