6981417a496d692f5fffe2f38721282b4468fb1d
punch <jkh1447@gmail.com>
2025-10-06 09:26:02 +0900

    [level 3] Title: 입국심사, Time: 47.64 ms, Memory: 6.75 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    
+    long long maxt = *max_element(times.begin(), times.end()) * n;
+    long long mint = *min_element(times.begin(), times.end());
+    long long st = mint;
+    long long en = maxt;
+    long long ans = 0;
+    
+    while(st <= en) {
+        long long mid = (st+en) / 2;
+        long long hap = 0;
+        for(int i=0; i<times.size(); i++){
+            hap += mid / times[i];
+        }
+        if(hap >= n) {
+            ans = mid;
+            en = mid-1;
+        }
+        else {
+            st = mid + 1;
+        }
+    }
+    
+    answer = ans;
+    
+    return answer;
+}
