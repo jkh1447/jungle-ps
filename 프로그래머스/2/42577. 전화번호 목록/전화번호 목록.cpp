d08b77ce755737bef99e61e104772ca1d47649e6
punch <jkh1447@gmail.com>
2025-10-08 18:04:51 +0900

    [level 2] Title: 전화번호 목록, Time: 74.96 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,22 +1,15 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <bits/stdc++.h>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-        unordered_set<string> hash;
-    for (auto& num : phone_book)
-        hash.insert(num);
+    sort(phone_book.begin(), phone_book.end());
     
-    for (auto& num : phone_book) {
-        string prefix = "";
-        for (int i = 0; i < num.size() - 1; i++) {
-            prefix += num[i];
-            if (hash.find(prefix) != hash.end())
-                return false;  // 접두어 발견
+    for(int i=0; i<phone_book.size()-1; i++){
+        if(phone_book[i+1].find(phone_book[i]) == 0){
+            return false;
         }
     }
     return true;
-
 } 
