6d8de39cec8ad82caeb2e0716c5d463c7b8a48cf
punch <jkh1447@gmail.com>
2025-11-22 15:14:38 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+#include <deque>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define LL long long
+
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+
+//int dx[6] = { 0, 1, 0, -1, 0, 0 };
+//int dy[6] = { 1, 0, -1, 0, 0, 0 };
+//int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    vector<int> dp(n+1);
+    dp[0] = 1;
+    dp[1] = 0;
+    dp[2] = 3;
+    for (int i = 4; i <= n; i+=2) {
+        dp[i] = dp[i - 2] * 3;
+        for (int j = i - 4; j >= 0; j -= 2) {
+            dp[i] += dp[j] * 2;
+        }
+    }
+    cout << dp[n];
+}
