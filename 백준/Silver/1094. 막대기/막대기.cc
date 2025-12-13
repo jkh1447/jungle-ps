19dd11e9b659b81776384a038a993b50cd524410
punch <jkh1447@gmail.com>
2025-12-13 15:08:30 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+#include <deque>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define LL long long
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+//int dx[6] = { 0, 1, 0, -1, 0, 0 };
+//int dy[6] = { 1, 0, -1, 0, 0, 0 };
+//int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+    int cnt = 0;
+    while (x > 0) {
+        if (x % 2 == 1) cnt++;
+        x /= 2;
+    }
+    cout << cnt;
+
+
+    
+}
+
+    
