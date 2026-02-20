0afd952b1d2dc99dc0eb7752e118a87aef0dd7ac
punch <jkh1447@gmail.com>
2026-02-20 14:44:07 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+#include <deque>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define LL long long
+
+//int dx[4] = { 1, 0, -1, 0 };
+//int dy[4] = { 0, 1, 0, -1 };
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+//int dx[6] = { 0, 1, 0, -1, 0, 0 };
+//int dy[6] = { 1, 0, -1, 0, 0, 0 };
+//int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+//int dx[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+//int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+int board[101][101];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        int n = b, m = a;
+
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
+                board[j + n][k + m] = 1;
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < 101; i++) {
+        for (int j = 0; j < 101; j++) {
+            if (board[i][j] == 1) ans++;
+        }
+    }
+    cout << ans;
+}
