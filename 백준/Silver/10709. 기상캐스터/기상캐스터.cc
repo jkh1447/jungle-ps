26dbaf09cfa861415130eb21a9b4ffb794af104e
punch <jkh1447@gmail.com>
2026-03-12 13:18:32 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+#include <deque>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define LL long long
+
+// 아래, 오른쪽, 위, 왼쪽
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+//int dx[4] = { -1, 0, 1, 0 };
+//int dy[4] = { 0, 1, 0, -1 };
+
+//int dx[6] = { 0, 1, 0, -1, 0, 0 };
+//int dy[6] = { 1, 0, -1, 0, 0, 0 };
+//int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+//int dx[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+//int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int n, m;
+char board[101][101];
+int dist[101][101];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    for (int i = 0; i < 101; i++)
+        fill(dist[i], dist[i] + 101, -1);
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < m; j++) {
+            board[i][j] = s[j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'c') {
+                dist[i][j] = 0;
+                for (int k = j + 1; k < m; k++) {
+                    if (board[i][k] == 'c') break;
+                    dist[i][k] = dist[i][k - 1] + 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
