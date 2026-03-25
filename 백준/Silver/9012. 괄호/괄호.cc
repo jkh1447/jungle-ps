7083333b93b99f58846141459fa0ea992e9ffd22
punch <jkh1447@gmail.com>
2026-03-25 22:10:34 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+#include <deque>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define LL long long
+
+// 아래, 오른쪽, 위, 왼쪽
+//int dx[4] = { 1, 0, -1, 0 };
+//int dy[4] = { 0, 1, 0, -1 };
+
+//int dx[4] = { -1, 0, 1, 0 };
+//int dy[4] = { 0, 1, 0, -1 };
+
+//int dx[6] = { 0, 1, 0, -1, 0, 0 };
+//int dy[6] = { 1, 0, -1, 0, 0, 0 };
+//int dz[6] = { 0, 0, 0, 0, 1, -1 };
+
+//int dx[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+//int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        stack<char> st;
+        string s;
+        cin >> s;
+        bool flag = true;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push('(');
+            }
+            else if (s[i] == ')') {
+                if (st.empty() || (!st.empty() && st.top() == ')')){
+                    flag = false;
+                    break;
+                }
+                if (!st.empty() && st.top() == '(') {
+                    st.pop();
+                }
+            }
+        }
+
+        if (!st.empty() || !flag) cout << "NO\n";
+        else cout << "YES\n";
+    }
+}
+
