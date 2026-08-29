633517f8cd23c4306eede395d6dfba40d3a88b50
punch <jkh1447@gmail.com>
2026-08-30 00:24:17 +0900

    [level 2] Title: 구명보트, Time: 1.51 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int s = 0, e = people.size() - 1;
+    sort(people.begin(), people.end());
+    while(s <= e) {
+        if (people[s] + people[e] <= limit) s++;
+        e--;
+        answer++;
+    }
+    return answer;
+}
