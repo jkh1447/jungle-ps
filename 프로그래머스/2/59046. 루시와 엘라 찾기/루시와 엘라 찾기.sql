bde4caa6498e43b69e9469164955c7f1b59afd36
punch <jkh1447@gmail.com>
2026-09-17 23:17:17 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,21 @@
+
+
+
+
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME = 'Lucy' OR NAME = 'Ella' OR NAME = 'Pickle' OR NAME = 'Rogan' OR NAME = 'Sabrina' OR NAME = 'Mitty'
-ORDER BY ANIMAL_ID 
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+ORDER BY ANIMAL_ID
+
+
+
+
+
+
+
+
+
+
+
+
+
